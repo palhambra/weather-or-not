c749a7b7b3afcaac6bd22324f8b2df826c8d22b2
Paolo <paolo.alhambra@yahoo.com>
2023-02-02 22:21:20 -0800

    Comments on plan for JavaScript

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+// When pressing search button, will make button of city and push to a list. City & info set to local storage. History city button will pull from local storage 
+// When search button is pressed, will make one big div of current day. And 5 smaller divs/cards of next 5 days
+// Add api link for weather
