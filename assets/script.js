14ada1c46cba41e28e8728b8b4714fd941bbee7f
Paolo <paolo.alhambra@yahoo.com>
2023-02-04 15:17:34 -0800

    Added button and text input for city search

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
-// When pressing search button, will make button of city and push to a list. City & info set to local storage. History city button will pull from local storage 
+// When pressing search button, will make button of city and push to a list. City & info set to local storage. History city button will pull from local storage
+var searchHistory = [];
 // When search button is pressed, will make one big div of current day. And 5 smaller divs/cards of next 5 days
 // Add api link for weather
